3da9c612ff47d6dd30fec2ed1922688799f522ae
SuleAktas <suleaktas@live.nl>
2024-04-20 15:59:31 +0300

    feat: fetch data for recipes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,91 +1,102 @@
 import HandPickedCollections from "../HandPickedCollections/HandPickedCollections";
 import PopularCategories from "../PopularCategories/PopularCategories";
 import RatedRecipe from "../RatedRecipe/RatedRecipe";
+import { Provider } from "react-redux";
+import { ApiProvider } from "@reduxjs/toolkit/query/react";
+
+import { store } from "../../store";
+import { recipesApi, useGetAllRecipesQuery } from "../../features/apiSlice";
 import "./App.css";
 
 function App() {
+  const { data } = useGetAllRecipesQuery();
+  console.log(data);
   const RECIPE = process.env.PUBLIC_URL + "/images/recipe.jpg";
   const COLLECTION1 = process.env.PUBLIC_URL + "/images/collection1.jpeg";
 
   return (
-    <div className="App">
-      <div className="super-delicious">
-        <span className="title">Super Delicious</span>
-        <div className="recipe-boxes">
-          <RatedRecipe
-            image={RECIPE}
-            title={"Spinach and Cheese Salad"}
-            rating={3.75}
-          />
-          <RatedRecipe
-            image={RECIPE}
-            title={"Spinach and Cheese Salad"}
-            rating={4.75}
-          />
-          <RatedRecipe
-            image={RECIPE}
-            title={"Spinach and Cheese Salad"}
-            rating={2.75}
-          />
-          <RatedRecipe
-            image={RECIPE}
-            title={"Spinach and Cheese Salad"}
-            rating={1.75}
-          />
-          <RatedRecipe
-            image={RECIPE}
-            title={"Spinach and Cheese Salad"}
-            rating={5.75}
-          />
+    <Provider store={store}>
+      <ApiProvider api={recipesApi}>
+        <div className="App">
+          <div className="super-delicious">
+            <span className="title">Super Delicious</span>
+            <div className="recipe-boxes">
+              <RatedRecipe
+                image={RECIPE}
+                title={"Spinach and Cheese Salad"}
+                rating={3.75}
+              />
+              <RatedRecipe
+                image={RECIPE}
+                title={"Spinach and Cheese Salad"}
+                rating={4.75}
+              />
+              <RatedRecipe
+                image={RECIPE}
+                title={"Spinach and Cheese Salad"}
+                rating={2.75}
+              />
+              <RatedRecipe
+                image={RECIPE}
+                title={"Spinach and Cheese Salad"}
+                rating={1.75}
+              />
+              <RatedRecipe
+                image={RECIPE}
+                title={"Spinach and Cheese Salad"}
+                rating={5.75}
+              />
+            </div>
+          </div>
+          <div className="collections">
+            <span className="collection-title2">Hand-Picked Collections</span>
+            <div className="collection-boxes">
+              <HandPickedCollections
+                image={COLLECTION1}
+                title={"Sushi Combos for your Next Party!"}
+                link={"156 Recipes"}
+              />
+              <HandPickedCollections
+                image={COLLECTION1}
+                title={"Sushi Combos for your Next Party!"}
+                link={"180 Recipes"}
+              />
+              <HandPickedCollections
+                image={COLLECTION1}
+                title={"Sushi Combos for your Next Party!"}
+                link={"157 Recipes"}
+              />
+              <HandPickedCollections
+                image={COLLECTION1}
+                title={"Sushi Combos for your Next Party!"}
+                link={"1 Recipes"}
+              />
+              <HandPickedCollections
+                image={COLLECTION1}
+                title={"Sushi Combos for your Next Party!"}
+                link={"55 Recipes"}
+              />
+              <HandPickedCollections
+                image={COLLECTION1}
+                title={"Sushi Combos for your Next Party!"}
+                link={"44 Recipes"}
+              />
+            </div>
+          </div>
+          <div className="popular-categories">
+            <div className="popular-title">Popular Categories</div>
+            <div className="popular-categories-boxes">
+              <PopularCategories />
+              <PopularCategories />
+              <PopularCategories />
+              <PopularCategories />
+              <PopularCategories />
+              <PopularCategories />
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="collections">
-        <span className="collection-title2">Hand-Picked Collections</span>
-        <div className="collection-boxes">
-          <HandPickedCollections
-            image={COLLECTION1}
-            title={"Sushi Combos for your Next Party!"}
-            link={"156 Recipes"}
-          />
-          <HandPickedCollections
-            image={COLLECTION1}
-            title={"Sushi Combos for your Next Party!"}
-            link={"180 Recipes"}
-          />
-          <HandPickedCollections
-            image={COLLECTION1}
-            title={"Sushi Combos for your Next Party!"}
-            link={"157 Recipes"}
-          />
-          <HandPickedCollections
-            image={COLLECTION1}
-            title={"Sushi Combos for your Next Party!"}
-            link={"1 Recipes"}
-          />
-          <HandPickedCollections
-            image={COLLECTION1}
-            title={"Sushi Combos for your Next Party!"}
-            link={"55 Recipes"}
-          />
-          <HandPickedCollections
-            image={COLLECTION1}
-            title={"Sushi Combos for your Next Party!"}
-            link={"44 Recipes"}
-          />
-        </div>
-      </div>
-      <div className="popular-categories">
-        <div className="popular-title">Popular Categories</div>
-        <div className="popular-categories-boxes">
-          <PopularCategories />
-          <PopularCategories />
-          <PopularCategories />
-          <PopularCategories />
-          <PopularCategories />
-          <PopularCategories />
-        </div>
-      </div>
-    </div>
+      </ApiProvider>
+    </Provider>
   );
 }
 
---
+++ src/features/apiSlice.js
@@ -0,0 +1,15 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const recipesApi = createApi({
+  reducerPath: "recipesApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://www.themealdb.com/api/json/v1/1/",
+  }),
+  endpoints: (builder) => ({
+    getAllRecipes: builder.query({
+      query: () => "search.php?f=a",
+    }),
+  }),
+});
+
+export const { useGetAllRecipesQuery } = recipesApi;
