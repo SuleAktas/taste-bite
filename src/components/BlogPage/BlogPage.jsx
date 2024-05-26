fe5682d90c8dfc318750af6f419aaa686f5f4bbb
SuleAktas <suleaktas@live.nl>
2024-05-26 14:02:28 +0300

    fix:lorem ipsum texts change

--- src/components/BlogPage/BlogPage.jsx
+++ src/components/BlogPage/BlogPage.jsx
@@ -32,7 +32,6 @@ function BlogPage() {
           Yesterday
         </div>
       </div>
-
       <div className="blog-pre-exp">
         One thing a learned living in the Canarsie section of Brooklyn, Ny was
         how to cook a good Italian meal.Here is recipe I created after having
@@ -44,34 +43,37 @@ function BlogPage() {
       <div className="blog-before">
         <div className="blog-general-title">Before you begin</div>
         <div className="blog-basics-exp">
-          Lorem ipsum lorem ipsum lorem ipsuml orem ipsum lorem ipsumlorem ipsum
-          lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
-          ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-          lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem.
+          In adipisicing quis deserunt est nostrud. Anim cupidatat aute in anim
+          amet minim id proident adipisicing laboris sunt aute. Sit deserunt et
+          deserunt non proident excepteur velit non elit excepteur fugiat ex
+          duis labore.um lorem.
         </div>
       </div>
-
       <div className="blog-basics">
         <div className="blog-basics-text">
           <div className="blog-general-title">Here are the basics</div>
           <div className="blog-basics-exp">
-            Lorem ipsum lorem ipsum lorem ipsuml orem ipsum lorem ipsumlorem
-            ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-            lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
-            ipsum lorem ipsum lorem ipsum lorem ipsum.
+            Dolore mollit pariatur eu tempor labore irure ut voluptate dolore
+            enim. Voluptate cupidatat est elit eu incididunt occaecat. Labore
+            labore ullamco do ea et ipsum occaecat exercitation dolor. Quis nisi
+            do anim aliqua ipsum irure exercitation laboris duis elit sit.
+            Proident exercitation incididunt ullamco laboris aliquip laboris
+            aute commodo duis id nisi. Proident enim excepteur est tempor
+            adipisicing est id adipisicing cillum consequat pariatur sint non
+            duis.
           </div>
           <div className="blog-basics-exp">
-            Lorem ipsum lorem ipsumlorem ipsum lorem ipsum lorem ipsum lorem
-            ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-            lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
-            ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-            lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem.
+            Et eu aliquip et pariatur do non non ad. Reprehenderit proident
+            tempor ullamco anim nostrud quis labore. Ex non enim cillum amet
+            voluptate laborum fugiat aliqua ipsum sunt. Nulla veniam cillum est
+            ex reprehenderit aliqua laborum est culpa Lorem dolore.
           </div>
         </div>
         <div className="blog-basics-img">
           <img src={DOUGH}></img>
         </div>
       </div>
+
       <div className="blog-writer">
         <div className="blog-writer-quote">
           "One cannot think well,love well, sleep well, if one has not dined
@@ -81,74 +83,68 @@ function BlogPage() {
           -Virginia Woolf, A Room of One's Own
         </div>
       </div>
-
       <div className="blog-kitchen">
         <div className="blog-general-title">In the kitchen</div>
         <div className="blog-basics-exp">
-          Lorem ipsum lorem ipsum lorem ipsuml orem ipsum lorem ipsumlorem ipsum
-          lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
-          ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-          lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
-          ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-          lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
-          ipsum lorem ipsum
+          Mollit adipisicing nisi ea qui Lorem eiusmod reprehenderit fugiat
+          veniam pariatur pariatur sunt nulla. Consequat Lorem mollit tempor
+          fugiat occaecat do magna ex. Nostrud in reprehenderit aliquip amet
+          amet eu id. Nulla cupidatat commodo cillum esse sit minim laborum elit
+          id nisi adipisicing proident. Qui culpa labore tempor minim sint
+          consectetur magna. Aute nisi eu aute aliqua dolore nisi aliqua
+          reprehenderit occaecat anim nulla ad. Duis ad non minim magna minim.
+          Ut officia consectetur dolore anim labore quis. Nisi ipsum consectetur
+          dolore laborum ipsum ut culpa Lorem.
         </div>
         <div className="blog-basics-exp">
-          Lorem ipsum lorem ipsum lorem ipsuml orem ipsum lorem ipsumlorem ipsum
-          lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
-          ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-          lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
-          ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-          lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
-          ipsum lorem ipsum
+          In adipisicing quis deserunt est nostrud. Anim cupidatat aute in anim
+          amet minim id proident adipisicing laboris sunt aute. Sit deserunt et
+          deserunt non proident excepteur velit non elit excepteur fugiat ex
+          duis labore.um lorem.
         </div>
         <div className="blog-basics-exp">
-          Lorem ipsum lorem ipsum lorem ipsuml orem ipsum lorem ipsumlorem ipsum
-          lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
-          ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-          lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
-          ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-          lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
-          ipsum lorem ipsum
+          Et exercitation aliqua commodo labore cillum eu Lorem. Consequat culpa
+          ea commodo sunt mollit. Ullamco sunt deserunt eu sunt cupidatat in
+          cupidatat nulla in id magna exercitation laboris. Occaecat enim
+          exercitation adipisicing deserunt consectetur amet. Excepteur occaecat
+          ad deserunt pariatur anim magna exercitation reprehenderit officia
+          tempor eiusmod non sunt do. Consectetur elit nostrud reprehenderit
+          nisi ut dolore ullamco aliqua ut tempor incididunt nostrud aliqua.
         </div>
         <div className="blog-kitchen-img">
           <img src={DISH}></img>
         </div>
         <div className="blog-basics-exp">
-          Lorem ipsum lorem ipsum lorem ipsuml orem ipsum lorem ipsumlorem ipsum
-          lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
-          ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-          lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
-          ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-          lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
-          ipsum lorem ipsum
+          Aliquip adipisicing nostrud culpa ex Lorem consectetur proident aute
+          elit ex cupidatat officia. Reprehenderit in qui voluptate et aliqua
+          laborum laborum aliquip est ex excepteur ad et. Nulla sit irure nisi
+          magna quis laborum labore ea. Deserunt occaecat nisi ut ullamco amet
+          aliquip excepteur velit aliqua duis. Aute proident sit consequat dolor
+          nulla amet reprehenderit. Laborum magna consectetur nulla est commodo
+          in enim dolore anim. Irure voluptate consequat quis velit duis velit
+          exercitation ipsum quis deserunt.
         </div>
         <div className="blog-basics-exp">
-          Lorem ipsum lorem ipsum lorem ipsuml orem ipsum lorem ipsumlorem ipsum
-          lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
-          ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-          lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
-          ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-          lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
-          ipsum
+          Consequat aute quis voluptate labore deserunt amet quis cupidatat
+          deserunt commodo est. Culpa culpa laborum proident commodo veniam
+          laboris. Culpa magna elit quis reprehenderit in officia culpa cillum
+          occaecat cillum do nostrud aliqua. Enim incididunt ipsum aute tempor
+          cillum deserunt aliqua aute id Lorem. Consectetur fugiat ullamco eu
+          enim.
         </div>
         <div className="blog-basics-exp">
-          Lorem ipsum lorem ipsum lorem ipsuml orem ipsum lorem ipsumlorem ipsum
-          lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
-          ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-          lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
-          ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-          lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
-          ipsum lorem
+          Dolore tempor cupidatat veniam laborum officia sit deserunt nisi ea
+          elit. Laboris mollit quis aliqua amet amet proident aliqua enim
+          reprehenderit. Reprehenderit aliqua ut irure laboris fugiat veniam
+          minim tempor qui ea incididunt.
         </div>
         <div className="blog-basics-exp">
-          Lorem ipsum lorem ipsum lorem ipsuml orem ipsum lorem ipsumlorem ipsum
-          lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
-          ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-          lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
-          ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-          lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
-          ipsum lorem ipsum lorem ipsum
+          Incididunt velit consectetur aliqua ad sint. Quis magna exercitation
+          do excepteur incididunt sint est nostrud sit aute ad fugiat. Do
+          voluptate laborum cupidatat cupidatat mollit dolor nostrud Lorem et
+          consequat. Deserunt deserunt aliquip minim consequat proident dolore.
+          Excepteur quis commodo fugiat eu. Consequat ex id in excepteur
+          reprehenderit ea fugiat ullamco consectetur ad magna.
         </div>
       </div>
       <div className="blog-author-info"></div>
