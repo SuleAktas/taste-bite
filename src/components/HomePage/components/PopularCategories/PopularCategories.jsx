78c41327739c3c7d6ea42f35510a0dfaeb64a0a2
SuleAktas <suleaktas@live.nl>
2024-05-27 14:37:39 +0300

    fix:popular category boxes get bigger

--- src/components/HomePage/components/PopularCategories/PopularCategories.jsx
+++ src/components/HomePage/components/PopularCategories/PopularCategories.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import "./PopularCategories.css";
 
 function PopularCategories(props) {
+  const getTitle = (title) => {
+    return title.length > 7 ? title.substring(0, 6) + "..." : title;
+  };
+
   return (
     <div className="category">
       <div className="category-image">
         <img src={props.image}></img>
       </div>
-      <div className="category-title">{props.title}</div>
+      <div className="category-title">{getTitle(props.title)}</div>
     </div>
   );
 }
