d13d7f175a48dd271fdc438d9aef7812754a8c9e
SuleAktas <suleaktas@live.nl>
2024-05-27 16:08:55 +0300

    fix:Link given to the boxes

--- src/components/HomePage/components/LatestRecipes/LatestRecipes.jsx
+++ src/components/HomePage/components/LatestRecipes/LatestRecipes.jsx
@@ -4,21 +4,22 @@ import { Link } from "react-router-dom";
 
 function LatestRecipes(props) {
   return (
-    <div className="latest-recipe-box">
-      <div className="latest-recipe-image">
-        <img src={props.image}></img>
-      </div>
-      <Link
-        to={{
-          pathname: "/recipe",
-          state: { recipeId: props.id },
-          search: `?id=${props.id}`,
-        }}
-        className="link-without-decoration"
-      >
+    <Link
+      to={{
+        pathname: "/recipe",
+        state: { recipeId: props.id },
+        search: `?id=${props.id}`,
+      }}
+      className="link-without-decoration"
+    >
+      <div className="latest-recipe-box">
+        <div className="latest-recipe-image">
+          <img src={props.image}></img>
+        </div>
+
         <div className="latest-recipe-title">{props.title}</div>
-      </Link>
-    </div>
+      </div>
+    </Link>
   );
 }
 
--- src/components/HomePage/components/RatedRecipe/RatedRecipe.jsx
+++ src/components/HomePage/components/RatedRecipe/RatedRecipe.jsx
@@ -12,40 +12,42 @@ function RatedRecipe(props) {
   }, []);
 
   return (
-    <div className="rated-recipe">
-      <div className="rated-recipe-img">
-        <img src={props.image}></img>
-      </div>
-      <div className="rated-recipe-stars">
-        <span
-          className={stars >= 1 ? "fa fa-star checked" : "fa fa-star"}
-        ></span>
-        <span
-          className={stars >= 2 ? "fa fa-star checked" : "fa fa-star"}
-        ></span>
-        <span
-          className={stars >= 3 ? "fa fa-star checked" : "fa fa-star"}
-        ></span>
-        <span
-          className={stars >= 4 ? "fa fa-star checked" : "fa fa-star"}
-        ></span>
-        <span
-          className={stars >= 5 ? "fa fa-star checked" : "fa fa-star"}
-        ></span>
-      </div>
-      <div className="rated-recipe-exp-box">
-        <div className="rated-recipe-title">{props.title}</div>
-        <Link
-          to={{
-            pathname: "/recipe",
-            state: { recipeId: props.id },
-            search: `?id=${props.id}`,
-          }}
-        >
+    <Link
+      to={{
+        pathname: "/recipe",
+        state: { recipeId: props.id },
+        search: `?id=${props.id}`,
+      }}
+      className="link-without-decoration"
+    >
+      <div className="rated-recipe">
+        <div className="rated-recipe-img">
+          <img src={props.image}></img>
+        </div>
+        <div className="rated-recipe-stars">
+          <span
+            className={stars >= 1 ? "fa fa-star checked" : "fa fa-star"}
+          ></span>
+          <span
+            className={stars >= 2 ? "fa fa-star checked" : "fa fa-star"}
+          ></span>
+          <span
+            className={stars >= 3 ? "fa fa-star checked" : "fa fa-star"}
+          ></span>
+          <span
+            className={stars >= 4 ? "fa fa-star checked" : "fa fa-star"}
+          ></span>
+          <span
+            className={stars >= 5 ? "fa fa-star checked" : "fa fa-star"}
+          ></span>
+        </div>
+        <div className="rated-recipe-exp-box">
+          <div className="rated-recipe-title">{props.title}</div>
+
           <i className="gg-arrow-right"></i>
-        </Link>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
