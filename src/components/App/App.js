ccaa5c1673c86a8b92a364b247afecfae3f9bd67
SuleAktas <suleaktas@live.nl>
2025-04-11 12:15:14 +0200

    fix:eslint add

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,29 +1,34 @@
-import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Header from "../Layout/Header/Header";
-import Footer from "../Layout/Footer/Footer";
-import HomePage from "../HomePage/HomePage";
-import BlogPage from "../BlogPage/BlogPage";
-import RecipePage from "../RecipePage/RecipePage";
-import CategoryPage from "../CategoryPage/CategoryPage";
-import RecipesPage from "../RecipesPage/RecipesPage";
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from '../Layout/Header/Header';
+import Footer from '../Layout/Footer/Footer';
+import HomePage from '../HomePage/HomePage';
+import BlogPage from '../BlogPage/BlogPage';
+import RecipePage from '../RecipePage/RecipePage';
+import CategoryPage from '../CategoryPage/CategoryPage';
+import RecipesPage from '../RecipesPage/RecipesPage';
 
 function App() {
-  return (
-    <Router>
-      <div className="app">
-        <Header />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/blog" element={<BlogPage />} />
-          <Route path="/categories" element={<CategoryPage />} />
-          <Route path="/recipes" element={<RecipesPage />} />
-          <Route path="/recipe" element={<RecipePage />} />
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
-  );
+	return (
+		<Router>
+			<div className="app">
+				<Header />
+				<Routes>
+					<Route path="/" element={<HomePage />} />
+					<Route path="/blog" element={<BlogPage />} />
+					<Route path="/categories" element={<CategoryPage />} />
+					<Route path="/recipes" element={<RecipesPage />} />
+					<Route path="/recipe" element={<RecipePage />} />
+				</Routes>
+				<Footer />
+
+
+
+
+        
+			</div>
+		</Router>
+	);
 }
 
 export default App;
