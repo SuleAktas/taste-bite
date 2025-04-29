a08b266f76e75dfbbb75aad53369fe1cbe2f839b
SuleAktas <suleaktas@live.nl>
2025-04-29 14:50:58 +0200

    fix:class change to className

--- src/components/Layout/Footer/Footer.jsx
+++ src/components/Layout/Footer/Footer.jsx
@@ -1,98 +1,98 @@
-import React from "react";
-import "./Footer.css";
-import Accordion from "react-bootstrap/Accordion";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTwitter } from "@fortawesome/free-brands-svg-icons";
-import { faInstagram } from "@fortawesome/free-brands-svg-icons";
-import { faFacebook } from "@fortawesome/free-brands-svg-icons";
-import { faYoutube } from "@fortawesome/free-brands-svg-icons";
+import React from 'react';
+import './Footer.css';
+import Accordion from 'react-bootstrap/Accordion';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faYoutube } from '@fortawesome/free-brands-svg-icons';
 
 function Footer() {
-  const LOGO = process.env.PUBLIC_URL + "/images/TasteBite.png";
-  return (
-    <div className="footer-box">
-      <div className="footer-top">
-        <div className="footer-first-part">
-          <div className="footer-logo">
-            <img src={LOGO} alt="logo"></img>
-          </div>
-          <div className="footer-text">
-            "On the other hand we denounce with righteous indignation and
-            dislike men who are so beguiled and demoralized by the charms of
-            pleasure of the moment"
-          </div>
-        </div>
-        <div className="footer-second-part">
-          <div className="footer-links">
-            <div className="links">
-              <div>
-                <h5>Tastebite</h5>
-                <button class="link-style">About Us</button>
-                <button class="link-style">Careers</button>
-                <button class="link-style">Contact Us</button>
-                <button class="link-style">Feedback</button>
-              </div>
-              <div>
-                <h5>Legal</h5>
-                <button class="link-style">Terms</button>
-                <button class="link-style">Conditions</button>
-                <button class="link-style">Cookies</button>
-                <button class="link-style">Copyright</button>
-              </div>
-              <div>
-                <h5>Follow</h5>
-                <button class="link-style">Facebook</button>
-                <button class="link-style">Twitter</button>
-                <button class="link-style">Instagram</button>
-                <button class="link-style">Youtube</button>
-              </div>
-            </div>
-            <Accordion className="custom-accordion" defaultActiveKey="0">
-              <Accordion.Item eventKey="0">
-                <Accordion.Header>Tastebite</Accordion.Header>
-                <Accordion.Body className="links">
-                  <button class="link-style">About Us</button>
-                  <button class="link-style">Careers</button>
-                  <button class="link-style">Contact Us</button>
-                  <button class="link-style">Feedback</button>
-                </Accordion.Body>
-              </Accordion.Item>
-              <Accordion.Item eventKey="1">
-                <Accordion.Header>Legal</Accordion.Header>
-                <Accordion.Body className="links">
-                  <button class="link-style">Terms</button>
-                  <button class="link-style">Conditions</button>
-                  <button class="link-style">Cookies</button>
-                  <button class="link-style">Copyright</button>
-                </Accordion.Body>
-              </Accordion.Item>
-              <Accordion.Item eventKey="2">
-                <Accordion.Header>Follow</Accordion.Header>
-                <Accordion.Body className="links">
-                  <button class="link-style">Facebook</button>
-                  <button class="link-style">Twitter</button>
-                  <button class="link-style">Instagram</button>
-                  <button class="link-style">Youtube</button>
-                </Accordion.Body>
-              </Accordion.Item>
-            </Accordion>
-          </div>
-        </div>
-      </div>
+	const LOGO = process.env.PUBLIC_URL + '/images/TasteBite.png';
+	return (
+		<div className="footer-box">
+			<div className="footer-top">
+				<div className="footer-first-part">
+					<div className="footer-logo">
+						<img src={LOGO} alt="logo"></img>
+					</div>
+					<div className="footer-text">
+						"On the other hand we denounce with righteous indignation and
+						dislike men who are so beguiled and demoralized by the charms of
+						pleasure of the moment"
+					</div>
+				</div>
+				<div className="footer-second-part">
+					<div className="footer-links">
+						<div className="links">
+							<div>
+								<h5>Tastebite</h5>
+								<button className="link-style">About Us</button>
+								<button className="link-style">Careers</button>
+								<button className="link-style">Contact Us</button>
+								<button className="link-style">Feedback</button>
+							</div>
+							<div>
+								<h5>Legal</h5>
+								<button className="link-style">Terms</button>
+								<button className="link-style">Conditions</button>
+								<button className="link-style">Cookies</button>
+								<button className="link-style">Copyright</button>
+							</div>
+							<div>
+								<h5>Follow</h5>
+								<button className="link-style">Facebook</button>
+								<button className="link-style">Twitter</button>
+								<button className="link-style">Instagram</button>
+								<button className="link-style">Youtube</button>
+							</div>
+						</div>
+						<Accordion className="custom-accordion" defaultActiveKey="0">
+							<Accordion.Item eventKey="0">
+								<Accordion.Header>Tastebite</Accordion.Header>
+								<Accordion.Body className="links">
+									<button className="link-style">About Us</button>
+									<button className="link-style">Careers</button>
+									<button className="link-style">Contact Us</button>
+									<button className="link-style">Feedback</button>
+								</Accordion.Body>
+							</Accordion.Item>
+							<Accordion.Item eventKey="1">
+								<Accordion.Header>Legal</Accordion.Header>
+								<Accordion.Body className="links">
+									<button className="link-style">Terms</button>
+									<button className="link-style">Conditions</button>
+									<button className="link-style">Cookies</button>
+									<button className="link-style">Copyright</button>
+								</Accordion.Body>
+							</Accordion.Item>
+							<Accordion.Item eventKey="2">
+								<Accordion.Header>Follow</Accordion.Header>
+								<Accordion.Body className="links">
+									<button className="link-style">Facebook</button>
+									<button className="link-style">Twitter</button>
+									<button className="link-style">Instagram</button>
+									<button className="link-style">Youtube</button>
+								</Accordion.Body>
+							</Accordion.Item>
+						</Accordion>
+					</div>
+				</div>
+			</div>
 
-      <div className="footer-bottom">
-        <div className="footer-social">
-          <FontAwesomeIcon icon={faFacebook} size="2x" color=" #888888" />
-          <FontAwesomeIcon icon={faTwitter} size="2x" color=" #888888" />
-          <FontAwesomeIcon icon={faInstagram} size="2x" color=" #888888" />
-          <FontAwesomeIcon icon={faYoutube} size="2x" color=" #888888" />
-        </div>
-        <div className="footer-rights">
-          @ 2024 Tastebite - All rights reserved
-        </div>
-      </div>
-    </div>
-  );
+			<div className="footer-bottom">
+				<div className="footer-social">
+					<FontAwesomeIcon icon={faFacebook} size="2x" color=" #888888" />
+					<FontAwesomeIcon icon={faTwitter} size="2x" color=" #888888" />
+					<FontAwesomeIcon icon={faInstagram} size="2x" color=" #888888" />
+					<FontAwesomeIcon icon={faYoutube} size="2x" color=" #888888" />
+				</div>
+				<div className="footer-rights">
+					@ 2024 Tastebite - All rights reserved
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Footer;
--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -1,90 +1,90 @@
-import React from "react";
-import "./Header.css";
-import { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
-import { faCircleUser } from "@fortawesome/free-solid-svg-icons";
+import React from 'react';
+import './Header.css';
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faCircleUser } from '@fortawesome/free-solid-svg-icons';
 
-import { faBars } from "@fortawesome/free-solid-svg-icons";
-import { Link } from "react-router-dom";
-import Search from "./components/Search/Search";
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
+import Search from './components/Search/Search';
 
 function Header() {
-  const LOGO = process.env.PUBLIC_URL + "/images/TasteBite.png";
+	const LOGO = process.env.PUBLIC_URL + '/images/TasteBite.png';
 
-  const [isOpen, setIsOpen] = useState(false);
+	const [isOpen, setIsOpen] = useState(false);
 
-  const openNav = () => {
-    setIsOpen(true);
-  };
+	const openNav = () => {
+		setIsOpen(true);
+	};
 
-  const closeNav = () => {
-    setIsOpen(false);
-  };
-  const [showSearch, setShowSearchComponent] = useState(false);
+	const closeNav = () => {
+		setIsOpen(false);
+	};
+	const [showSearch, setShowSearchComponent] = useState(false);
 
-  const handleClick = () => {
-    setShowSearchComponent(true);
-  };
+	const handleClick = () => {
+		setShowSearchComponent(true);
+	};
 
-  const handleClose = () => {
-    setShowSearchComponent(false);
-  };
+	const handleClose = () => {
+		setShowSearchComponent(false);
+	};
 
-  return (
-    <div className="header-box">
-      <img className="header-logo" src={LOGO} alt="logo"></img>
-      <div className="links-wider-page">
-        <Link to="/">Home</Link>
-        <Link to="/blog">Blog</Link>
-        <Link to="/categories">Categories</Link>
-        <Link to="/buy">Buy</Link>
-      </div>
-      <div className="wider-page-icons">
-        <FontAwesomeIcon
-          icon={faSearch}
-          size="xl"
-          onClick={handleClick}
-          style={{
-            cursor: "pointer",
-          }}
-        />
-        <FontAwesomeIcon icon={faCircleUser} size="xl" />
-      </div>
-      <div className="header-buttons">
-        <FontAwesomeIcon
-          icon={faSearch}
-          size="xl"
-          onClick={handleClick}
-          style={{
-            cursor: "pointer",
-          }}
-        />
-        <FontAwesomeIcon icon={faBars} size="xl" onClick={openNav} />
-      </div>
+	return (
+		<div className="header-box">
+			<img className="header-logo" src={LOGO} alt="logo"></img>
+			<div className="links-wider-page">
+				<Link to="/">Home</Link>
+				<Link to="/blog">Blog</Link>
+				<Link to="/categories">Categories</Link>
+				<Link to="/buy">Buy</Link>
+			</div>
+			<div className="wider-page-icons">
+				<FontAwesomeIcon
+					icon={faSearch}
+					size="xl"
+					onClick={handleClick}
+					style={{
+						cursor: 'pointer',
+					}}
+				/>
+				<FontAwesomeIcon icon={faCircleUser} size="xl" />
+			</div>
+			<div className="header-buttons">
+				<FontAwesomeIcon
+					icon={faSearch}
+					size="xl"
+					onClick={handleClick}
+					style={{
+						cursor: 'pointer',
+					}}
+				/>
+				<FontAwesomeIcon icon={faBars} size="xl" onClick={openNav} />
+			</div>
 
-      {showSearch && (
-        <div className="search-overlay">
-          <div className="search-container">
-            <Search onClose={handleClose} />
-          </div>
-        </div>
-      )}
-      <div
-        id="mySidenav"
-        className={`sidenav ${isOpen ? "open" : ""}`}
-        style={{ width: isOpen ? "250px" : "0" }}
-      >
-        <button class="closebtn " onClick={closeNav}>
-          &times;
-        </button>
-        <Link to="/">Home</Link>
-        <Link to="/blog">Blog</Link>
-        <Link to="/categories">Categories</Link>
-        <Link to="/buy">Buy</Link>
-      </div>
-    </div>
-  );
+			{showSearch && (
+				<div className="search-overlay">
+					<div className="search-container">
+						<Search onClose={handleClose} />
+					</div>
+				</div>
+			)}
+			<div
+				id="mySidenav"
+				className={`sidenav ${isOpen ? 'open' : ''}`}
+				style={{ width: isOpen ? '250px' : '0' }}
+			>
+				<button className="closebtn " onClick={closeNav}>
+					&times;
+				</button>
+				<Link to="/">Home</Link>
+				<Link to="/blog">Blog</Link>
+				<Link to="/categories">Categories</Link>
+				<Link to="/buy">Buy</Link>
+			</div>
+		</div>
+	);
 }
 
 export default Header;
