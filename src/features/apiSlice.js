2ad57b5b9f9f03549818fa1ffdbf5ec6b6d60a2d
SuleAktas <suleaktas@live.nl>
2025-04-29 15:52:00 +0200

    feat:endpoint change

--- src/features/apiSlice.js
+++ src/features/apiSlice.js
@@ -1,37 +1,41 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const recipesApi = createApi({
-  reducerPath: "recipesApi",
-  baseQuery: fetchBaseQuery({
-    baseUrl: "https://www.themealdb.com/api/json/v1/1/",
-  }),
-  endpoints: (builder) => ({
-    getAllRecipes: builder.query({
-      query: () => "search.php?f=b",
-    }),
-    getAllCategories: builder.query({
-      query: () => "categories.php",
-    }),
-    getRandomMeal: builder.query({
-      query: () => "random.php",
-    }),
-    getMealById: builder.query({
-      query: (id) => `lookup.php?i=${id}`,
-    }),
-    getRecipesByCategory: builder.query({
-      query: (name) => `filter.php?c=${name}`,
-    }),
-    getRecipesByName: builder.query({
-      query: (name) => `search.php?f=${name}`,
-    }),
-  }),
+	reducerPath: 'recipesApi',
+	baseQuery: fetchBaseQuery({
+		baseUrl: 'http://localhost:8081/api/',
+	}),
+	endpoints: builder => ({
+		getAllRecipes: builder.query({
+			query: () => 'recipe',
+		}),
+		getAllCategories: builder.query({
+			query: () => 'category',
+		}),
+		getRandomMeal: builder.query({
+			query: () => 'recipe/popularRecipe',
+		}),
+		getMealById: builder.query({
+			query: id => `recipe/id=${id}`,
+		}),
+		getRecipesByCategory: builder.query({
+			query: name => `filter.php?c=${name}`,
+		}),
+		getRecipesByName: builder.query({
+			query: name => `search.php?f=${name}`,
+		}),
+		getAllCollections: builder.query({
+			query: () => `collection`,
+		}),
+	}),
 });
 
 export const {
-  useGetAllCategoriesQuery,
-  useGetAllRecipesQuery,
-  useGetRandomMealQuery,
-  useGetMealByIdQuery,
-  useGetRecipesByCategoryQuery,
-  useGetRecipesByNameQuery,
+	useGetAllCategoriesQuery,
+	useGetAllRecipesQuery,
+	useGetRandomMealQuery,
+	useGetMealByIdQuery,
+	useGetRecipesByCategoryQuery,
+	useGetRecipesByNameQuery,
+	useGetAllCollectionsQuery,
 } = recipesApi;
